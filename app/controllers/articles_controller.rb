0c884b95631386d0b6b3e41437a8d4de84255c5e
vishal patidar <vishalpatidar963027@gmail.com>
2021-01-08 18:49:08 +0530

    implement styling with bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
     # byebug
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
 
   def index
@@ -13,11 +15,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title,:description))
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
@@ -27,8 +29,8 @@ class ArticlesController < ApplicationController
 
   def update
     # byebug
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title,:description))
+    #@article = Article.find(params[:id])
+    if @article.update(article_params)
       redirect_to @article
     else
       render 'edit'
@@ -36,8 +38,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title,:description)
   end
 end
